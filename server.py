8d5cef4266f554af358544dcd1423bddc7a5d1dc
Seunghui Choi <63492803+jaragos0s@users.noreply.github.com>
2021-05-31 07:46:21 +0900

    server 완성본

--- server.py
+++ server.py
@@ -5,8 +5,10 @@ import threading
 
 
 class ChatServer:
-
+    # 연결된 클라이언트 정보 저장하는 리스트
     clients_list = []
+    
+    # 재고 초기값 ( GUI 구현 위함 )
     americano = 30
     waffle = 30
     latte = 30
@@ -14,27 +16,27 @@ class ChatServer:
     coldbrew=30
     espresso=30
     prafe=30
-    last_received_message = ""
-
+    
+    last_received_info = ""
+    
     def __init__(self, americano = 30, waffle = 30, latte = 30,smoothie=30,coldbrew=30,espresso=30,prafe=30):
-        
         self.create_listening_server()
         self.americano = americano
         self.waffle = waffle
         self.latte = latte
-        self.smoothie=30
-        self.coldbrew=30
-        self.espresso=30
-        self.prafe=30
+        self.smoothie= smoothie
+        self.coldbrew= coldbrew
+        self.espresso= espresso
+        self.prafe= prafe
 
-    def update(self, menu, n,senders_socket):
-        print('update', menu, n)
+    # 재고 변경 함수 menu : 메뉴이름, n : 변경 개수
+    def update(self, menu, n):
         if menu == "americano":
-            if self.americano>0:
-                self.americano -= int(n)
-                if self.americano == 0:
-                    text="Now "+menu+" is sold out!"
-                    for client in self.clients_list:
+            if self.americano>0: # 아메리카노 재고 있으면 
+                self.americano -= int(n) # 하나를 뺌
+                if self.americano == 0: # 하나 빼고 난 후 재고 없으면
+                    text="Now "+menu+" is sold out!" # 품절 메시지
+                    for client in self.clients_list: # 모든 클라이언트에게 품절되었음을 알림
                         socket, (ip, port) = client
                         socket.sendall(text.encode('utf-8'))
             
@@ -87,60 +89,69 @@ class ChatServer:
                         socket, (ip, port) = client
                         socket.sendall(text.encode('utf-8'))
 
+    # 서버 소켓 생성
     def create_listening_server(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        local_ip = '220.66.218.156'
-        local_port = 50005
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 소켓 생성
+        local_ip = '220.66.218.156' # 서버 ip
+        local_port = 50005 # 포트 번호
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
         self.server_socket.bind((local_ip, local_port))
-        print("Listening for incoming messages..")
-        self.server_socket.listen(5)
-        self.receive_messages_in_a_new_thread()
+        print("Listening for connect..")
+        self.server_socket.listen(5) # 최대 5개 소켓 연결 가능
+        self.receive_info_in_a_new_thread()
 
-    def receive_messages(self, so):
+    # 클라이언트로부터 메시지 받음
+    def receive_info(self, so):
         while True:
             incoming_buffer = so.recv(256)
             if not incoming_buffer:
                 break
             
-            self.last_received_message = incoming_buffer.decode('utf-8')
+            # 클라이언트로 받은 문자 decoding
+            self.last_received_info = incoming_buffer.decode('utf-8')
             
-            user, t = map(str, self.last_received_message.split(':'))
+            # 문자 parsing(user이름, 메뉴이름, 변경 개수)
+            user, t = map(str, self.last_received_info.split(':'))
             menu, n = map(str, t.split())
             
-            self.update(menu, n,so)
-            self.send_numbers(so)
+            # 메뉴 변경
+            self.update(menu, n)
+            # 클라이언트에게 재고 변경 알림
+            self.send_numbers()
             
         so.close()
-    def send_numbers(self,senders_socket):
+    # 클라이언트에 재고 보냄 (아메리카노 와플 스무디 콜드브루 에스프레소 프라페)
+    def send_numbers(self):
         text=" "+str(self.americano)+" "+str(self.latte)+" "+str(self.waffle)+" "+str(self.smoothie)+" "+str(self.coldbrew)+" "+str(self.espresso)+" "+str(self.prafe)
         for client in self.clients_list:
             socket,(ip,port)=client
             socket.sendall(text.encode('utf-8'))
+
+    # 다른 클라이언트들에게 정보 보냄
     def broadcast_to_all_clients(self, senders_socket):
         for client in self.clients_list:
             socket, (ip, port) = client
             if socket is not senders_socket:
+                socket.sendall(self.last_received_info.encode('utf-8'))
                 
-                socket.sendall(self.last_received_message.encode('utf-8'))
-                
-                
-                
-
-    def receive_messages_in_a_new_thread(self):
+    # 클라이언트와 소켓 연결
+    def receive_info_in_a_new_thread(self):
         while 1:
+            # accept
             client = so, (ip, port) = self.server_socket.accept()
+            # 클라이언트 목록에 추가
             self.add_to_clients_list(client)
             print ('Connected to ', ip, ':', str(port))
             self.broadcast_to_all_clients(so)
-
-            t = threading.Thread(target=self.receive_messages, args=(so,))
+            # 쓰레드 생성
+            t = threading.Thread(target=self.receive_info, args=(so,))
             t.start()
 
-
+    # 클라이언트 목록을 추가하는 함수
     def add_to_clients_list(self, client):
         if client not in self.clients_list:
             self.clients_list.append(client)
 
+# 메인 함수
 if __name__ == "__main__":
     ChatServer()
